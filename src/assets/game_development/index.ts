fa10de6dddea63e519feb9ea1bd9e80a7b517e4f
mahbubhossine <138327493+mahbubhossine@users.noreply.github.com>
2026-07-23 21:09:18 +0600

    Add files via upload

---
+++ src/assets/game_development/index.ts
@@ -0,0 +1,66 @@
+// ───────── Game Development Portfolio Assets ─────────
+// All image imports from the game_development folder
+import imgBreakBricks from "./breakBricksNeonMobile.png";
+import imgGame5 from "./I-will-create-game-with-javascript-react-phaser-create-a-flappy-bird-style-endless-game-create-your-unity-2d-game-create-a-custom-flappy-bird-style-game-develop-a-casual-mobile-game-development2d-archery-drawing-game-html5-game-5.png";
+import imgGame6 from "./I-will-create-game-with-javascript-react-phaser-create-a-flappy-bird-style-endless-game-create-your-unity-2d-game-create-a-custom-flappy-bird-style-game-develop-a-casual-mobile-game-development2d-archery-drawing-game-html5-game-6.png";
+import imgGameConveJpg from "./I-will-create-game-with-javascript-react-phaser-create-a-flappy-bird-style-endless-game-create-your-unity-2d-game-create-a-custom-flappy-bird-style-game-develop-a-casual-mobile-game-development2d-archery-drawing-game-html5-game-conve.jpg";
+import imgGameConvePng from "./I-will-create-game-with-javascript-react-phaser-create-a-flappy-bird-style-endless-game-create-your-unity-2d-game-create-a-custom-flappy-bird-style-game-develop-a-casual-mobile-game-development2d-archery-drawing-game-html5-game-conve.png";
+import imgGameConve2 from "./I-will-create-game-with-javascript-react-phaser-create-a-flappy-bird-style-endless-game-create-your-unity-2d-game-create-a-custom-flappy-bird-style-game-develop-a-casual-mobile-game-development2d-archery-drawing-game-html5-game-conve2.jpg";
+import imgLiveSpinWin from "./LiveSpinAndWin.png";
+import imgRocketRush from "./rocket rush mania.png";
+import imgScreenshot from "./Screenshot 2025-10-30 130516.png";
+
+// ───────── Portfolio Items for Game Development ─────────
+export interface GamePortfolioItem {
+  img: string;
+  name: string;
+  cat: "Game Development";
+}
+
+export const gamePortfolioItems: GamePortfolioItem[] = [
+  {
+    img: imgBreakBricks,
+    name: "Break Bricks Neon",
+    cat: "Game Development",
+  },
+  {
+    img: imgGame5,
+    name: "Bubble Shooter",
+    cat: "Game Development",
+  },
+  {
+    img: imgGame6,
+    name: "Snake Game",
+    cat: "Game Development",
+  },
+  {
+    img: imgGameConveJpg,
+    name: "Flappy Bird Sky",
+    cat: "Game Development",
+  },
+  {
+    img: imgGameConvePng,
+    name: "Wack A Mole",
+    cat: "Game Development",
+  },
+  {
+    img: imgGameConve2,
+    name: "Archary",
+    cat: "Game Development",
+  },
+  {
+    img: imgLiveSpinWin,
+    name: "Live Spining Wheel",
+    cat: "Game Development",
+  },
+  {
+    img: imgRocketRush,
+    name: "Rocket Rush Mania",
+    cat: "Game Development",
+  },
+  {
+    img: imgScreenshot,
+    name: "Break Bricks Dextop",
+    cat: "Game Development",
+  },
+];
